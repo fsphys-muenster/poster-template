df8b7f302e2916b6136e0fba2077e8e1367db3ba
Simon May <simon.may@uni-muenster.de>
2018-08-20 03:11:43 +0200

    Reorganize structure, adding build script and README.md

---
+++ README.md
@@ -0,0 +1,46 @@
+# titlepage-template [DE]
+Diese Dateien bilden die Grundlage für die Vorlage für wissenschaftliche Poster
+im Corporate Design der Westfälischen Wilhelms-Universität Münster (WWU).
+Die Vorlage selbst ist unter
+https://sso.uni-muenster.de/intern/marketing/informationsmedien/printvorlagen.html
+zu finden.
+
+Eine fertige `.zip`-Datei mit Vorlagen auf Deutsch und Englisch sowie allen
+Beispiel-Präsentationen erhält man durch ausführen der Datei [`build.sh`](build.sh).
+
+## Hinweis zu den Schriftarten
+Zur Verwendung der Vorlage wird die Schriftart „Meta Office Pro“ von FontFont
+(LinoType GmbH) benötigt. Dazu müssen die Dateien
+- MetaOffcPro-Bold.ttf
+- MetaOffcPro-Norm.ttf
+- MetaOffcPro-NormIta.ttf
+- MetaScOffcPro-Bold.ttf
+- MetaScOffcPro-Norm.ttf
+- MetaScOffcPro-NormIta.ttf
+
+im Verzeichnis [`fonts/`](tex/fonts/) abgelegt werden.
+
+---
+
+# titlepage-template [EN]
+These files form the basis for the template for scientific posters adhering to
+the corporate design of Westfälische Wilhelms-Universität Münster (WWU).
+The template itself can be found using the link
+https://sso.uni-muenster.de/intern/marketing/informationsmedien/printvorlagen.html
+within the WWU internal area.
+
+A complete `.zip` file with templates in German and English as well as all
+example presentations can be obtained by running [`build.sh`](build.sh).
+
+## A note on fonts
+In order to use the template, the font “Meta Office Pro” by FontFont (LinoType GmbH)
+is required. That is, the files
+- MetaOffcPro-Bold.ttf
+- MetaOffcPro-Norm.ttf
+- MetaOffcPro-NormIta.ttf
+- MetaScOffcPro-Bold.ttf
+- MetaScOffcPro-Norm.ttf
+- MetaScOffcPro-NormIta.ttf
+
+must be placed into the directory [`fonts/`](tex/fonts/).
+
---
+++ build.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+# Generates a complete .zip file with templates in German and English as well
+# as all example title posters (all possible colors and sizes).
+# Note: This takes quite a long time due to the amount of processed TeX
+# documents.
+# Command line arguments:
+# $1: the LaTeX engine used for compilation (lualatex or xelatex);
+#     optional (default: lualatex)
+
+texdir=tex
+fontdir=tex/fonts
+zipname=latex_wwu_poster_$(date +%F)
+
+latex_engine=$1
+case "$latex_engine" in
+	lualatex|xelatex)
+		;;
+	'')
+		latex_engine=lualatex
+		;;
+	*)
+		echo "Invalid LaTeX engine (must be lualatex or xelatex): $latex_engine"
+		exit 2
+		;;
+esac
+if [ ! -f "$fontdir/MetaOffcPro-Bold.ttf" ]     || \
+	[ ! -f "$fontdir/MetaOffcPro-Norm.ttf" ]    || \
+	[ ! -f "$fontdir/MetaOffcPro-NormIta.ttf" ] || \
+	[ ! -f "$fontdir/MetaScOffcPro-Bold.ttf" ]  || \
+	[ ! -f "$fontdir/MetaScOffcPro-Norm.ttf" ]  || \
+	[ ! -f "$fontdir/MetaScOffcPro-NormIta.ttf" ]; then
+	echo "Font files missing in $fontdir/!"
+	exit 1
+fi
+
+# clean up previous build
+rm -rf "$zipname/"
+# remove converted PDF versions of EPS files
+rm -f "$texdir/wwustyle"/*eps-converted-to*
+
+olddir="$PWD"
+
+tempdir=$(mktemp -d /tmp/latex-beamer.XXXXXX)
+cp -r "$texdir/" "$tempdir/"
+cd $tempdir
+# delete dummy file
+find "$fontdir/" -type f ! -name '*.ttf' -delete
+
+# set up directories for compilation of example PDFs
+mkdir -p de/vorlage/
+mkdir -p en/template/
+
+cp -r "$fontdir/" "$texdir/wwustyle/" \
+	"$texdir"/*.sty \
+	"$texdir/poster.tex" \
+	'de/vorlage/'
+cp -r "$fontdir/" "$texdir/wwustyle/" \
+	"$texdir"/*.sty \
+	"$texdir/poster.tex" \
+	'en/template/'
+
+cp -r de/vorlage/ de/beispiele/
+mv de/beispiele/poster.tex de/beispiele/poster.tex
+cp -r en/template/ en/examples/
+
+rm -r "$texdir/"
+
+# compile examples
+for d in de/beispiele en/examples; do
+	cd "$d/"
+	# generate all size and color options
+	for size in a0paper a1paper; do
+		for color in black7 234 7462 312 315 3135 369 390; do
+			options="pantone$color"
+			# remove commas from file names
+			filename=$(echo "${options}_${size}" | sed 's/,/_/g')
+			cp poster.tex "$filename.tex"
+			# set options in the TeX file
+			sed -i "s/a1paper]{article}/$size]{article}/" "$filename.tex"
+			sed -i "s/pantone312]/$options]/" "$filename.tex"
+			# run compilations
+			latexmk "-$latex_engine" -interaction=nonstopmode -silent \
+				"$filename.tex"
+		done
+	done
+	# remove source files
+	rm -r $(basename "$fontdir/") wwustyle/ *.tex *.sty
+	# remove auxiliary files
+	rm *.aux *.log *.out *.toc *.fls *.fdb_latexmk *.synctex.gz
+	cd "$tempdir"
+done
+
+# create .zip file
+cd "$olddir"
+cp -r "$tempdir/" "$zipname"
+zip -r "$zipname.zip" "$zipname/"
+
+rm -r "$tempdir"
+
